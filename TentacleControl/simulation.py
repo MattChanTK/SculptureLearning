ce55d4a13ae48d592bd17dda0159517daf8ab6f5
tuzzer <matthew.tk.chan@gmail.com>
2013-12-12 03:07:38 -0500

    Simple code to test if the Tentacle actually go to where if supposes to given num of SMA activation as input

--- TentacleControl/simulation.py
+++ TentacleControl/simulation.py
@@ -8,6 +8,12 @@ sim_step = 0.01
 # instantiating the Tentacle
 plant = Tentacle.Tentacle()
 
+print 'initial angle: %f' % plant.get_angle()
+
+for t in range(num_step):
+    plant.update(1, sim_step=sim_step)
+    print '[%4d] angle: %f' % (t+1, plant.get_angle())
+
 
 
 
