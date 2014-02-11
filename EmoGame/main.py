f112118d8c96b08867fe1334d5ee384dca893e38
tuzzer <matthew.tk.chan@gmail.com>
2014-02-11 15:16:39 -0500

    Ball game

--- EmoGame/main.py
+++ EmoGame/main.py
@@ -1,31 +1,26 @@
-from direct.showbase.ShowBase import ShowBase
-from direct.task import Task
+import sys
+import pygame
 import math
 
-class EmoGame(ShowBase):
+size = width, height = 800, 500
+speed = [1, 1]
+black = 0, 0, 0
 
-    def __init__(self):
-        ShowBase.__init__(self)
+screen = pygame.display.set_mode(size)
 
-        # Load the environment model.
-        self.environ = self.loader.loadModel("models/environment")
-        # Reparent the model to render.
-        self.environ.reparentTo(self.render)
-        # Apply scale and position transforms on the model.
-        self.environ.setScale(0.25, 0.25, 0.25)
-        self.environ.setPos(-10, 0, -10)
+ball = pygame.image.load("ball.gif")
+ballrect = ball.get_rect()
 
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
 
-        # Add the spinCameraTask procedure to the task manager.
-        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
 
-    def spinCameraTask(self, task):
-        ang = task.time*60.0
-        angRad = math.radians(ang)
-        self.camera.setPos(20*math.sin(angRad), -20*math.cos(angRad),3)
-        self.camera.setHpr(ang, 0, 0)
-        return Task.cont
-
-game = EmoGame()
-game.run()
-
+    screen.fill(black)
+    screen.blit(ball,ballrect)
+    pygame.display.flip()
