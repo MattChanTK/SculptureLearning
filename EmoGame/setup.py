356c7507e1555f81ba858bf1f3e3d2f46b5fb743
tuzzer <matthew.tk.chan@gmail.com>
2014-02-12 19:29:55 -0500

    Adding the pictures

---
+++ EmoGame/setup.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import pygame
+import math
+
+from pygame.locals import *
+if not pygame.font: print 'Warning, fonts disabled'
+if not pygame.mixer: print 'Warning, sound disabled'
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error, message:
+        print 'Cannot load image:', name
+        raise SystemExit, message
+    #image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0,0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image, image.get_rect()
+
+
+def load_sound(name):
+    class NoneSound:
+        def play(self): pass
+    if not pygame.mixer:
+        return NoneSound()
+    fullname = os.path.join('data', name)
+    try:
+        sound = pygame.mixer.Sound(fullname)
+    except pygame.error, message:
+        print 'Cannot load sound:', wav
+        raise SystemExit, message
+    return sound
+
+num_spirit = 10
+bg_colour = (230, 240, 250)
+
+size = (600, 400)
+spirit_size = (20, 20)
+candy_up_size = (60, 60)
+candy_down_size = (40, 40)
+candy_factor_rate = 0.002
+
