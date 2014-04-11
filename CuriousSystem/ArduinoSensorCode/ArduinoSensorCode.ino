73d7c4b60cde4c6d2e252b4488a2e02fec2efc49
tuzzer <matthew.tk.chan@gmail.com>
2014-04-11 12:57:18 -0400

    Calibrating for 2s so that the heart beat properly captured.

--- CuriousSystem/ArduinoSensorCode/ArduinoSensorCode.ino
+++ CuriousSystem/ArduinoSensorCode/ArduinoSensorCode.ino
@@ -1,12 +1,13 @@
-const int gsr = a0;
-const int pulse = a1;
+const int gsr = A0;
+const int pulse = A1;
 
 // heart rate parameters
-const int hrWindow = 10;
+const int hrWindow = 10000;
 unsigned int pulseCount = 0;
 unsigned long hrTime = 0;
 double hr = millis();
 int pulseHigh = 0;
+boolean pulseEdgeUp = false;
 
 // current time
 unsigned long currTime = 0;
@@ -22,23 +23,29 @@ void setup()
   Serial.begin(9600);  
   
   //set up pulse sensor input
+  ground(pulse);
+  pinMode(pulse, INPUT);  
   
   //calibrating
-  Serial.println("Calibrating Pulse Sensor");
-  double sum = 0;
-  int maxVal = 0;
-  int minVal = 1024;
-  for (int i = 0; i < 100; ++i)
+  Serial.println("Calibrating Pulse Sensor...");
+  unsigned long sum = 0;
+  unsigned int maxVal = 0;
+  unsigned int minVal = 1024;
+  const unsigned int calDuration = 120;
+  for (int i = 0; i < calDuration; ++i)
   {
     int val = analogRead(pulse);
     sum += val;
     maxVal = max(maxVal, val);
     minVal = min(minVal, val);
+    delay(10);
   }
-  pulseHigh = sum/100;
-  Serial.println("Avg: " + String(pulseHigh));
+  unsigned int avgVal = sum/calDuration;
+  Serial.println("Avg: " + String(avgVal));
   Serial.println("Min: " + String(minVal));
   Serial.println("Max: " + String(maxVal));
+  pulseHigh = (maxVal + avgVal)>>1;
+  Serial.println("Pulse Threshold " + String(pulseHigh));
     
   
 }
@@ -50,13 +57,24 @@ void loop()
   // updating hr every (hrWindow) seconds
   if (currTime - hrTime > hrWindow)
   {
-    hr = (double)pulseCount/(double)(currTime - hrTime);
+    Serial.println(pulseCount);
+    hr = (double)pulseCount/((double)(currTime - hrTime)*1000);
     hrTime = millis();
+    pulseCount = 0; //reset count
   }
   
-  //if votlage drop below certain threshold
+  //Detecting pulses
   int val = analogRead(pulse);
-  //Serial.println(val);
+  if (val > pulseHigh && pulseEdgeUp == false)
+  {
+    pulseEdgeUp = true;
+  }
+  else if (val < pulseHigh && pulseEdgeUp == true)
+  {
+    pulseEdgeUp = false;
+    ++pulseCount;
+  }
+  //Serial.println(hr);
   
   
   
