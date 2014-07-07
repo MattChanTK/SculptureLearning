3425b48ef7c032f2c1a4926e71a5224bdef59c3a
tuzzer <matthew.tk.chan@gmail.com>
2014-07-07 12:57:54 -0400

    Added algorithm steps in comments

--- CuriousSystem2/main.py
+++ CuriousSystem2/main.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 # ===== Settings ======
 input_num = 1
 input_name = ["sensor"]
@@ -6,4 +6,38 @@ output_num = 1
 output_name = ["actuator"]
 
 # ==== Global variables =====
-input_val = []
+input_val = np.zeros(input_num)
+output_val = np.zeros(output_num)
+
+
+# ==== Algorithm ======
+
+# ---- initialize ----
+
+# ---- read sensor inputs ----
+
+# ---- extract features ----
+
+# LOOP START
+
+# ---- select action ----
+
+# ---- predict features ----
+
+# ---- generate actuator output commands ----
+
+# ---- write actuator outputs -----
+
+# ---- read sensor inputs ----
+
+# ---- extract features ----
+
+# ---- calculate prediction error ----
+
+# ---- add to training set -----
+
+# ---- calculate learning progress -----
+
+# ---- update learner ----
+
+# LOOP END
