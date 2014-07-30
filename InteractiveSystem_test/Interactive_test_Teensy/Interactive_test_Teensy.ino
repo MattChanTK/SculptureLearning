97d6824412dfc1a299ea033413f7066350c212db
tuzzer <matthew.tk.chan@gmail.com>
2014-07-30 17:46:33 -0400

    added an if statement so that it won't update the parameter list every loop on the arduino. Instead, only does so when there is a new message

--- InteractiveSystem_test/Interactive_test_Teensy/Interactive_test_Teensy.ino
+++ InteractiveSystem_test/Interactive_test_Teensy/Interactive_test_Teensy.ino
@@ -7,11 +7,6 @@ const unsigned int period = 0;
 //==== pin assignments ====
 const unsigned short indicator_led_pin = 13;
 
-//==== protocal specification ====
-const unsigned short indicator_led_on_byte[] = {1, 1};
-const unsigned short indicator_led_period_byte[] = {2, 2};
-
-
 //==== internal global variables =====
 byte outgoingByte[numOutgoingByte];
 byte incomingByte[numIncomingByte];
@@ -38,7 +33,7 @@ void blinkLED(void){
     digitalWrite(indicator_led_pin, ledState);  
 }
 
-void receive_msg(byte data_buff[]){
+boolean receive_msg(byte data_buff[]){
   
     noInterrupts();
     unsigned short byteCount = RawHID.recv(data_buff, 0);
@@ -47,52 +42,74 @@ void receive_msg(byte data_buff[]){
     if (byteCount > 0) {
       // compose reply message
       send_msg(data_buff);
+      return true;
+    }
+    else{
+      return false;
     }
 }
 
 void send_msg(byte data_buff[]){
    // Send a message
    noInterrupts();
-   RawHID.send(data_buff, 50);
+   RawHID.send(data_buff, 10);
    interrupts();
 }
+
+void parse_msg(byte data_buff[]){
+  
+  // byte 1 --- indicator led on or off
+  indicator_led_on = incomingByte[1];
+  
+  // byte 2 and 3 --- indicator led blinking frequency
+  int val = 0;
+  for (int i = 0; i < 2 ; i++)
+    val += incomingByte[i+2] << (8*i);
+  indicator_led_blinkPeriod = val*1000;
+  
+}
+
 
 void loop() {
   
   
   // check for new messages
-   receive_msg(incomingByte);
+   if (receive_msg(incomingByte)){
    
-   indicator_led_on = incomingByte[indicator_led_on_byte[0]];
+     // parse the message and save to parameters
+     parse_msg(incomingByte);
    
-   //ledState = indicator_led_on;
-   //digitalWrite(indicator_led_pin, ledState);  
-   int val = 0;
-   for (int i = 0; i <indicator_led_period_byte[1] ; i++)
-     val += incomingByte[i+indicator_led_period_byte[0]] << (8*i);
-   indicator_led_blinkPeriod = val*1000;
-   
-   if (indicator_led_on == 1){
-     if (indicator_led_blinkPeriod != indicator_led_blinkPeriod_0){
-       
-       indicator_led_blinkPeriod_0 = indicator_led_blinkPeriod;
-       
-       if (indicator_led_blinkPeriod > 0){
-         indicator_led_blinkTimer.begin(blinkLED, indicator_led_blinkPeriod);
-       }
-       else if (indicator_led_blinkPeriod == 0){
-         indicator_led_blinkTimer.end();
-         ledState = 1;
-         digitalWrite(indicator_led_pin, ledState);
+     //----the behaviour codes----
+     
+     //..... indicator LED .....
+     // if it should be on
+     if (indicator_led_on == 1){
+       // if there is a change in blink period
+       if (indicator_led_blinkPeriod != indicator_led_blinkPeriod_0){
+         
+         indicator_led_blinkPeriod_0 = indicator_led_blinkPeriod;
+         
+         //update the blinker's period
+         if (indicator_led_blinkPeriod > 0){
+           indicator_led_blinkTimer.begin(blinkLED, indicator_led_blinkPeriod);
+         }
+         //if the period is 0 just end the blink timer and and turn it on 
+         else if (indicator_led_blinkPeriod == 0){
+           indicator_led_blinkTimer.end();
+           ledState = 1;
+           digitalWrite(indicator_led_pin, ledState);
+         }
        }
      }
-   }
-   else{
-     indicator_led_blinkTimer.end();
-     ledState = 0;
-     indicator_led_blinkPeriod = -1;
-     indicator_led_blinkPeriod_0 = -1;
-     digitalWrite(indicator_led_pin, ledState);
+     // if it should be off
+     else{ 
+       // end the blink timer and turn it off
+       indicator_led_blinkTimer.end();
+       ledState = 0;
+       indicator_led_blinkPeriod = -1;
+       indicator_led_blinkPeriod_0 = -1;
+       digitalWrite(indicator_led_pin, ledState);
+     }
    }
    
 
